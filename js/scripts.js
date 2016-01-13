d00b04136dc7ec4c1bbcc578544db544ce3d648d
Christoph <cwolfanderson@gmail.com>
2016-01-13 11:18:25 -0800

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -73,14 +73,57 @@ $(function() {
     dialogbox.style.top = "100px";
     dialogbox.style.display = "block";
     document.getElementById('dialogboxhead').innerHTML = roomId;
-    document.getElementById('dialogboxbody').innerHTML = "<iframe src=pages/popup.html width=200 height=200></iframe>";
+    // document.getElementById('dialogboxbody').innerHTML =
+    // "<div class='panel panel-info'>" +
+    // "<p>Mode:" +
+    // "<select name='mode' id='mode'>" +
+    // "<option value=''>-</option>" +
+    // "<option value='party'>party</option>" +
+    // "<option value='lock-down'>lock down</option>" +
+    // "<option value='study'>study</option>" +
+    // "</select>" +
+    // "</p>" +
+    // "<p>Light:" +
+    // "<select name='light' id='light'>" +
+    // "<option value=''>-</option>" +
+    // "<option value='low'>low</option>" +
+    // "<option value='medium'>medium</option>" +
+    // "<option value='high'>high</option>" +
+    // "</select>" +
+    // "<input type='color' id='roomColor'>" +
+    // "</p>" +
+    // "<p>Temperature: <strong><span id='temperature'></span> &deg; <strong>" +
+    // "<input id='tempSlider' type='range' name='temperature' min='60' max='100' onchange='updateTemperature(this.value)'>" +
+    // "</p>" +
+    // "<button onclick='alterRoom()'>Commit</button>" +
+    // "</div>";
 
     document.getElementById('dialogboxfoot').innerHTML = '<button onclick="ok()">OK</button>';
 
     this.ok = function(){
+
+      // remove pop-up
       document.getElementById('dialogbox').style.display = "none";
       document.getElementById('dialogoverlay').style.display = "none";
+
+      // check mode
+
+      //console.log(document.getElementById('dialogbox'));
+
+      console.log($("#mode"));
+      console.log($("#mode").val());
+
+      // check light color""
+      console.log($("#light-color").val());
+      console.log($("#roomId"));
+      if ($("#light-color").val()) {
+        $("#roomId").backgroundColor = $("#light-color").val();
+      }
+
+      // check temperature
+
     };
+
   }
 
 });
