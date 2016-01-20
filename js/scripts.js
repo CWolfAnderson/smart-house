9493c3776774587a751525647d352a9ea37f2b17
ChristophAnderson <CWolfAnderson@gmail.com>
2016-01-19 18:40:48 -0800

    what?!

--- js/scripts.js
+++ js/scripts.js
@@ -72,31 +72,8 @@ $(function() {
     dialogbox.style.left = (winW/2) - (550 * 0.5) + "px";
     dialogbox.style.top = "100px";
     dialogbox.style.display = "block";
+
     document.getElementById('dialogboxhead').innerHTML = roomId;
-    // document.getElementById('dialogboxbody').innerHTML =
-    // "<div class='panel panel-info'>" +
-    // "<p>Mode:" +
-    // "<select name='mode' id='mode'>" +
-    // "<option value=''>-</option>" +
-    // "<option value='party'>party</option>" +
-    // "<option value='lock-down'>lock down</option>" +
-    // "<option value='study'>study</option>" +
-    // "</select>" +
-    // "</p>" +
-    // "<p>Light:" +
-    // "<select name='light' id='light'>" +
-    // "<option value=''>-</option>" +
-    // "<option value='low'>low</option>" +
-    // "<option value='medium'>medium</option>" +
-    // "<option value='high'>high</option>" +
-    // "</select>" +
-    // "<input type='color' id='roomColor'>" +
-    // "</p>" +
-    // "<p>Temperature: <strong><span id='temperature'></span> &deg; <strong>" +
-    // "<input id='tempSlider' type='range' name='temperature' min='60' max='100' onchange='updateTemperature(this.value)'>" +
-    // "</p>" +
-    // "<button onclick='alterRoom()'>Commit</button>" +
-    // "</div>";
 
     document.getElementById('dialogboxfoot').innerHTML = '<button onclick="ok()">OK</button>';
 
