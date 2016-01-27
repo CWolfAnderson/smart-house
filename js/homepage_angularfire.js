a43394a4501a39b0fba9edd11a52836108a34f86
ChristophAnderson <CWolfAnderson@gmail.com>
2016-01-26 18:32:12 -0800

    Add room colors

--- js/homepage_angularfire.js
+++ js/homepage_angularfire.js
@@ -1,22 +1,26 @@
 console.clear();
 (function(){
 
-var app = angular.module('smartHouse', ['firebase']);
+  var app = angular.module('smartHouse', ['firebase']);
 
-app.controller('houseController', ['$scope', '$firebaseObject',
-function ($scope, $firebaseObject) {
+  app.controller('houseController', ['$scope', '$firebaseObject',
+  function ($scope, $firebaseObject) {
 
-  var ref = new Firebase('https://smart-house.firebaseio.com/');
+    var ref = new Firebase('https://smart-house.firebaseio.com/');
 
-  // download users to local object
-  $scope.house = $firebaseObject(ref);
+    // download users to local object
+    $scope.house = $firebaseObject(ref);
 
-  // $scope.users = $scope.house.users;
+    // $scope.users = $scope.house.users;
 
-  $scope.house.$loaded().then(function() {
-	setTimeout(function(){ console.log($(".draggable")); $(".draggable").draggable(); }, 0);
-  });
-  }
-]);
+    $scope.house.$loaded().then(function() {
+      setTimeout(function(){ console.log($(".draggable")); $(".draggable").draggable(); }, 0);
+    });
+
+    $scope.roomClick = function(roomId) {
+      console.log("room: " + roomId);
+    };
+
+  }]);
 
 })();
--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,6 @@ $(function() {
     console.log(value);
   }
 
-  // create rooms
-  var rooms = [];
-
-  var room = {
-    name: "",
-    occupants: [],
-    temp: "",
-    music: {on: false, source: "", volume: 0},
-    light: {on: false, color: "", intensity: 0}
-    // possibly blinds
-  };
-
-  // create users
-  var users = [];
-
-  // function userCreator(name, priority, posX, posY, ) {
-  //
-  // }
-
-  var user = {
-    id: 1,
-    name: "",
-    priority: 0, // 0 - 10
-    status: "resident", // enum: {friend, unknown, resident, restricted}
-    location: "", // roomId, outside, or away
-    posX: 0,
-    posY: 0
-  };
-
   // allow popover
   $('[data-toggle="popover"]').popover({html: true});
 
@@ -67,65 +38,5 @@ $(function() {
     }
 
   });
-
-  $(".room").click(function(el) {
-    // get room id & forward it to popup
-    popUp(el.toElement.id);
-  });
-
-  // for pop-up
-  function popUp(roomId){
-
-    // get & set current room color
-    var roomColor = $("#" + roomId).css('backgroundColor');
-    $("#light-color").val(roomColor);
-
-    // get current temperature
-    var currTemp = $("#" + roomId + " > .temp-display").val();
-    $("#temp-slider").val(currTemp);
-
-    var winW = window.innerWidth;
-    var winH = window.innerHeight;
-    var dialogoverlay = document.getElementById('dialogoverlay');
-    var dialogbox = document.getElementById('dialogbox');
-    dialogoverlay.style.display = "block";
-    dialogoverlay.style.height = winH + "px";
-    dialogbox.style.left = (winW / 2) - (550 * 0.5) + "px";
-    dialogbox.style.top = "100px";
-    dialogbox.style.display = "block";
-    document.getElementById('dialogboxhead').innerHTML = roomId;
-    document.getElementById('dialogboxfoot').innerHTML = '<button onclick="ok()">OK</button>';
-
-    this.ok = function(){
-
-      // remove pop-up
-      document.getElementById('dialogbox').style.display = "none";
-      document.getElementById('dialogoverlay').style.display = "none";
-
-      // check mode
-      console.log($("#mode"));
-      console.log($("#mode").val());
-
-      // check light color""
-      console.log($("#light-color").val());
-
-      if ($("#light-color").val() != "#000000") { // if light color is not black
-        $("#" + roomId).css("background-color", $("#light-color").val());
-
-        // reset color of color picker
-        $("#light-color").val("#000000");
-      } else {
-        console.log("light is black");
-      }
-
-      // check temperature
-      if ($("#temp-slider").val() !== "") {
-        $("#" + roomId + " > .temp-display").html("<p class=temp-display>" + $("#temp-slider").val() + "&deg;</p>");
-      }
-
-    };
-
-  } // end popup
-
 
 });
