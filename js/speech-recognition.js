b5f786ba8e4c21c1d767150abace3ea46b3a2cac
ChristophAnderson <CWolfAnderson@gmail.com>
2016-01-29 11:22:46 -0800

    accident

--- js/speech-recognition.js
+++ js/speech-recognition.js
@@ -15,6 +15,17 @@ $(function() {
           alert("Turning on all the lights...");
           console.log("Turning on lights...");
         }
+//         // annyang will capture anything after a splat (*) and pass it to the function.
+// // e.g. saying "Show me Batman and Robin" will call showFlickr('Batman and Robin');
+// 'show me *tag': showFlickr,
+//
+// // A named variable is a one word variable, that can fit anywhere in your command.
+// // e.g. saying "calculate October stats" will call calculateStats('October');
+// 'calculate :month stats': calculateStats,
+//
+// // By defining a part of the following command as optional, annyang will respond
+// // to both: "say hello to my little friend" as well as "say hello friend"
+// 'say hello (to my little) friend': greeting
       };
 
       // Add our commands to annyang
