58660efaecf4a6204ca9828e5d9ca795b4a6f180
ChristophAnderson <CWolfAnderson@gmail.com>
2016-01-29 15:53:53 -0800

    This may break something

--- js/homepage_angularfire.js
+++ js/homepage_angularfire.js
@@ -7,30 +7,30 @@ console.clear();
   function ($scope, $firebaseObject) {
 
     var ref = new Firebase('https://smart-house.firebaseio.com/');
-	//var usersRef = new Firebase('https://smart-house.firebaseio.com/users/');
+    //var usersRef = new Firebase('https://smart-house.firebaseio.com/users/');
 
     // download users to local object
-	$scope.ref = ref;
+    $scope.ref = ref;
     $scope.house = $firebaseObject(ref);
     $scope.house.$loaded().then(function() {
       setTimeout(function(){ 
-		$('[data-toggle="popover"]').popover({html: true}); 
-		$(".draggable").draggable({
-			stop: function(event, ui) {
-				$scope.house.users[event.target.id].posX = ui.position.left;
-				$scope.house.users[event.target.id].posY = ui.position.top;
-				$scope.house.$save();
-			}
-		});
-		for (var key in $scope.house.users) {
-			if($scope.house.users.hasOwnProperty(key)) {
-				$("#"+key).css({'top': $scope.house.users[key].posY, 'left' : $scope.house.users[key].posX});
-			}
-		}
-		ref.child("users").on('child_changed', function(childSnapshot, prevChildKey) {
-			$("#"+childSnapshot.key()).css({'top': childSnapshot.val().posY, 'left' : childSnapshot.val().posX});
-		});
-	  }, 0);
+        $('[data-toggle="popover"]').popover({html: true});
+        $(".draggable").draggable({
+          stop: function(event, ui) {
+            $scope.house.users[event.target.id].posX = ui.position.left;
+            $scope.house.users[event.target.id].posY = ui.position.top;
+            $scope.house.$save();
+          }
+        });
+        for (var key in $scope.house.users) {
+          if($scope.house.users.hasOwnProperty(key)) {
+            $("#"+key).css({'top': $scope.house.users[key].posY, 'left' : $scope.house.users[key].posX});
+          }
+        }
+        ref.child("users").on('child_changed', function(childSnapshot, prevChildKey) {
+          $("#"+childSnapshot.key()).css({'top': childSnapshot.val().posY, 'left' : childSnapshot.val().posX});
+        });
+      }, 0);
     });
 
     $scope.roomClick = function(roomId) {
